841c2fd73dff4fa6d745ca68790d2caba0e85a9f
Guilherme Henrique <gh.dourado77@gmail.com>
2023-05-19 13:21:14 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
-/* eslint-disable prettier/prettier */
 import 'dotenv/config'
 
 import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
-import { authRoutes } from './routes/Auth'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/Auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
+
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,10 +27,14 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
-app.listen({
-  port:3333,
-}).then(()=> {
-  console.log("🚀 HTTP server running on port http://localhost:3333")
-})
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
